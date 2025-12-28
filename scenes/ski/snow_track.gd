2bbfaee827ce0b9c1d2bff4ec1123ad64da8353f
haohaomiao <haohaomiao@outlook.com>
2025-12-28 20:25:45 +0800

    complete start scene

--- scenes/ski/snow_track.gd
+++ scenes/ski/snow_track.gd
@@ -10,22 +10,23 @@ var recycle_margin := 100.0               # 安全余量（防止露缝）
 
 # 每帧
 func _process(delta):
-	var step := -dir * speed * delta as Vector2
-	track_a.position += step
-	track_b.position += step
-	
-	# 用投影标量判断前后（沿 dir 越大越“在前方”）
-	var ta := track_a.position.dot(dir)
-	var tb := track_b.position.dot(dir)
+    var step := -dir * speed * delta as Vector2
+    track_a.position += step
+    track_b.position += step
+    
+    # 用投影标量判断前后（沿 dir 越大越“在前方”）
+    var ta := track_a.position.dot(dir)
+    var tb := track_b.position.dot(dir)
 
-	var front = track_a
-	var back  = track_b
-	var t_front = ta
-	var t_back  = tb
-	if tb > ta:
-		front = track_b; back = track_a
-		t_front = tb;  t_back = ta
+    var front = track_a
+    var back  = track_b
+    var t_front = ta
+    var t_back  = tb
+    if tb > ta:
+        front = track_b; back = track_a
+        t_front = tb;  t_back = ta
 
-	# 回收条件：back 比 front 落后超过一段长度（加 margin）
-	if (t_front - t_back) >= (segment_len + recycle_margin):
-		back.position = front.position + dir * segment_len
+    # 回收条件：back 比 front 落后超过一段长度（加 margin）
+    if (t_front - t_back) >= (segment_len + recycle_margin):
+        print(t_front - t_back)
+        back.position = front.position + dir * segment_len
